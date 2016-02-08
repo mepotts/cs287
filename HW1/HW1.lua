21c28843b2da97b745b68c423a2c93ca73619808
Charles Liu <charlesliu@college.harvard.edu>
2016-02-07 23:58:54 -0500

    multiclass logistic regression, SGD not abstracted

--- HW1/HW1.lua
+++ HW1/HW1.lua
@@ -11,6 +11,9 @@ cmd:option('-classifier', 'nb', 'classifier to use')
 -- ...
 cmd:option('-alpha', 0, 'smoothing value')
 cmd:option('-lambda', 0, 'L2 regularization value')
+cmd:option('-m', 100, 'size of minibatch for gradient descent')
+cmd:option('-eta', 1, 'learning rate')
+cmd:option('-epochs', 100, 'number of epochs for gradient descent')
 
 
 -- predicts the class from the linear model specified by the parameters W and b
@@ -105,37 +108,70 @@ function learn_naive_bayes(alpha)
 	return W, b
 end
 
+function log_sum_exp(z)
+	local M = z:max(1)[1]
+	local ret = z:clone()
+	ret:csub(M)
+	return (torch.log(torch.exp(ret):sum(1)) + M)[1]
+end
+
 function softmax(x, W, b)
-	-- local z = 
+	local x = x[x:gt(1)]
+	local z = b:clone()
+	if x:dim() ~= 0 then
+		z:add(W:index(2, x:long()):sum(2))
+	end
+	
+	local denom = log_sum_exp(z)
+	z:csub(denom)
+	return torch.exp(z)
 end
 
 function learn_multiclass_logistic(lambda, m, eta, epochs)
 	local W = torch.zeros(nclasses, nfeatures)
 	local b = torch.zeros(nclasses)
 	
-	local i = 0
-	while i < epochs do
+	local n = 0
+	while n < epochs do
+		print("Iteration", n)
 		local sample = torch.multinomial(torch.ones(train_input:size(1)), m, false)
 		
-		local grad_W = torch.zeros(nclasses, nfeatures)
-		local grad_b = torch.zeros(nclasses)
+		local grad_W_hat = torch.zeros(nclasses, nfeatures)
+		local grad_b_hat = torch.zeros(nclasses)
 		
-		-- for i = 1:sample:size(1) do
-		-- 	local x = train_input[i]
+		for i = 1, sample:size(1) do
+			local x = train_input[sample[i]][train_input[sample[i]]:gt(1)]
 			
-		-- 	local y_hat = 
-		-- 	local class = train_output[sample[i]]
-		-- 	grad_b[]
-		-- end
-		-- sample
-		-- gradient mean = 0
-		-- for i = 1 to numsamples
-			-- compute loss
-			-- compute gradient
-			-- update gradient mean
-		-- update gradient
+			local y_hat = softmax(x, W, b)
+			local class = train_output[sample[i]]
+			
+			-- local grad = torch.ones(nclasses):mul(y_hat)
+			local grad = y_hat
+			grad[class] = grad[class] - 1
+			-- grad:mul(1/m)
+			
+			if x:dim() ~= 0 then
+				local grad_W = torch.zeros(nclasses, nfeatures)
+				for j = 1, x:size(1) do
+					col = grad_W:select(2, x[j])
+					col:add(grad)
+				end
+				grad_W:div(m)
+				grad_W_hat:add(grad_W)
+			end
+			
+			grad:div(m)
+			grad_b_hat:add(grad)
+		end
+		
+		W:mul(1 - eta * lambda / train_input:size(1))
+		b:mul(1 - eta * lambda / train_input:size(1))
+		
+		W:csub(grad_W_hat:mul(eta))
+		b:csub(grad_b_hat:mul(eta))
+		
+		n = n + 1
 	end
-	
 	
 	return W, b
 end
@@ -153,8 +189,11 @@ function main()
    valid_output = f:read("valid_output"):all()
    test_input = f:read("test_input"):all()   
    alpha = opt.alpha
-   lamdba = opt.lambda
-   
+   lambda = opt.lambda
+   m = opt.m
+   eta = opt.eta
+   epochs = opt.epochs
+      
 
    local W = torch.DoubleTensor(nclasses, nfeatures)
    local b = torch.DoubleTensor(nclasses)
@@ -163,7 +202,8 @@ function main()
 
    -- Train.
    
-   W, b = learn_naive_bayes(alpha)
+   -- W, b = learn_naive_bayes(alpha)
+   W, b = learn_multiclass_logistic(lambda, m, eta, epochs)
    
    -- Test.
    eval_linear_model(W, b)
